3c13d6bb0fe33ebe3feffa61d1bcdddc33a2af38
Prante524 <2221948@iub.edu.bd>
2025-08-31 03:13:09 +0600

    allow only logged-in Nutritionists

---
+++ nutritionist_profile.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$conn = require_once 'config.php';
+
+// Protect this page: allow only logged-in Nutritionists
+if (!isset($_SESSION['userID']) || $_SESSION['role'] !== 'Nutritionist') {
+    header("Location: login.php");
+    exit();
+}
+
+$nutritionistID = $_SESSION['userID'];
+$errorMsg = "";
