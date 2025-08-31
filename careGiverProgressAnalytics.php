4a8268246e42be7c2371668622837c5579f8add4
MoriumIslam <152001270+MoriumIslam@users.noreply.github.com>
2025-08-31 13:59:34 +0600

    30-min session timeout

--- careGiverProgressAnalytics.php
+++ careGiverProgressAnalytics.php
@@ -8,3 +8,12 @@ ini_set('session.cookie_secure', isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] =
 session_start();
 $conn = require_once 'config.php';
 date_default_timezone_set('Asia/Dhaka');
+// 30-min session timeout
+$timeout = 1800;
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout) {
+    session_unset();
+    session_destroy();
+    header("Location: login.php?error=Session expired. Please login again");
+    exit();
+}
+$_SESSION['last_activity'] = time();
