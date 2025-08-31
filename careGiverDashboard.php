a5c759002e9047919f118690b4dcfbbc383a2b63
MoriumIslam <152001270+MoriumIslam@users.noreply.github.com>
2025-08-31 13:52:21 +0600

    handle notification

--- careGiverDashboard.php
+++ careGiverDashboard.php
@@ -12,3 +12,13 @@ if (!isset($_SESSION['userID']) || $_SESSION['role'] !== 'CareGiver') {
 $careGiverID = $_SESSION['userID'];
 $userName = $_SESSION['Name'];
 $userAvatar = strtoupper(substr($userName, 0, 2));
+// --- Handle Dismissing a Notification ---
+if (isset($_GET['dismiss_notification_id'])) {
+    $notificationID = (int)$_GET['dismiss_notification_id'];
+    $stmt = $conn->prepare("UPDATE notification SET status = 'Read' WHERE notificationID = ? AND userID = ?");
+    $stmt->bind_param("ii", $notificationID, $careGiverID);
+    $stmt->execute();
+    $stmt->close();
+    header("Location: careGiverDashboard.php");
+    exit();
+}
